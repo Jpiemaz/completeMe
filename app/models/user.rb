a3586b5d75aa196eeb3d61654a3ebdabb1059d3a
John Mazzoli <jmazzoli@gmail.com>
2014-04-29 21:58:00 -0400

    Create and view tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  has_many :tasks
   has_attached_file :avatar, default_url: :default_avatar
   validates_attachment :avatar,
     content_type: {content_type: "/\Aimage\/.*\Z/"}
 
+  private
+
+  def default_avatar
+    "/default-avatar.png"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428193320) do
+ActiveRecord::Schema.define(version: 20140429203859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name",        null: false
+    t.text     "description"
+    t.date     "due_date",    null: false
+    t.boolean  "complete"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tasks", ["name"], name: "index_tasks_on_name", using: :btree
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at",                      null: false
