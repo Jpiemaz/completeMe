da95f01deb402134eb9868427f3154116f2dced5
John Mazzoli <jmazzoli@gmail.com>
2014-05-01 10:34:47 -0400

    Follow and unfollow other users

---
+++ app/controllers/following_relationships_controller.rb
@@ -0,0 +1,19 @@
+class FollowingRelationshipsController < ApplicationController
+  def create
+    followed_user = find_user
+    following_relationship = current_user.follow(followed_user)
+    redirect_to [followed_user, :profile]
+  end
+
+  def destroy
+    followed_user = find_user
+    current_user.unfollow(followed_user)
+    redirect_to [followed_user, :profile]
+  end
+
+  private
+
+  def find_user
+    User.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,36 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
+  has_many :followed_user_relationships,
+    foreign_key: :follower_id,
+    class_name: "FollowingRelationship"
+
+  has_many :followed_users,
+    through: :followed_user_relationships
+
+  has_many :follower_relationships,
+    foreign_key: :followed_user_id,
+    class_name: "FollowingRelationship"
+
+  has_many :followers,
+    through: :follower_relationships
+
   has_many :tasks
   has_attached_file :avatar, default_url: :default_avatar
   validates_attachment :avatar,
     content_type: {content_type: "/\Aimage\/.*\Z/"}
+
+  def follow(other_user)
+    followed_user_relationships.create(followed_user: other_user)
+  end
+
+  def unfollow(other_user)
+    followed_users.destroy(other_user)
+  end
+
+  def following?(other_user)
+    followed_user_ids.include? other_user.id
+  end
 
   private
 
