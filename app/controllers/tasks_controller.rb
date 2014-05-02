6825374d549ccebc1459efabb0506cbde6ed16aa
John Mazzoli <jmazzoli@gmail.com>
2014-05-02 09:51:25 -0400

    Update task view page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   def index
     @user = find_user
     @tasks = @user.tasks
+    @due_dates = due_dates
   end
 
   def new
@@ -27,5 +28,15 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).
       permit(:name, :description, :due_date)
+  end
+
+  def due_dates
+    user = find_user
+    tasks = user.tasks
+    due_dates = []
+    tasks.each do |task|
+      due_dates << task.due_date
+    end
+    due_dates.uniq.sort
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  def week_day_due
+    Date::DAYNAMES[self.due_date.wday]
+  end
+
+  def calendar_day_due
+    due_date.strftime("%B #{self.due_date.day.ordinalize}, %Y")
+  end
 end
