2ec506692b306217f6241663c508694f174048bf
John Mazzoli <jmazzoli@gmail.com>
2014-05-06 11:23:22 -0400

    Add avatars to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
   time_for_a_boolean(:completed)
+  has_attached_file :avatar
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   belongs_to :user
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501135613) do
+ActiveRecord::Schema.define(version: 20140506145039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,12 +28,16 @@ ActiveRecord::Schema.define(version: 20140501135613) do
 
   create_table "tasks", force: true do |t|
     t.integer  "user_id"
-    t.string   "name",         null: false
+    t.string   "name",                null: false
     t.text     "description"
-    t.date     "due_date",     null: false
+    t.date     "due_date",            null: false
     t.datetime "completed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "tasks", ["name"], name: "index_tasks_on_name", using: :btree
