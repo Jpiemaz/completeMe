c547468579559d15b595c03ffb58bb28b380b769
John Mazzoli <jmazzoli@gmail.com>
2014-05-09 18:18:29 -0400

    Display dates depending on due_date

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -10,4 +10,14 @@ module TasksHelper
   def date_completed(task)
     task.completed_at.strftime("%B #{task.completed_at.day.ordinalize}, %Y")
   end
+
+  def pretty_due_date(due_date)
+    if Time.now.to_date == due_date.to_date
+      "Today"
+    elsif Time.now < due_date && due_date < Time.now + 1.weeks
+      Date::DAYNAMES[due_date.wday]
+    else
+      due_date.strftime("%B #{due_date.day.ordinalize}, %Y")
+    end
+  end
 end
